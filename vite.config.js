a67bc1e32d9045ebaa6201bf1adddf31cf0821dc
FlyingFaller <bengsaunders@gmail.com>
2026-07-31 16:36:09 -0700

    Configured action to build and deploy to pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/curriculum-mapper/',
   plugins: [
     tailwindcss(),
   ],
